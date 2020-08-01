30f5ae21a37a713e80d28637de1226f992ca7077
CAM603 <cameronhawley@comcast.net>
2020-08-01 09:58:07 -0400

    fetching api

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
-import styles from "../styles/Home.module.css";
+import Card from "components/Card";
 
-export default function Home() {
-	return <div className={styles.container}>HI</div>;
+// import styles from "../styles/Home.module.css";
+import fetch from "isomorphic-unfetch";
+
+export default function Home({ movies }) {
+	return (
+		<div className="container">
+			{movies.map((movie) => (
+				<Card key={movie.id} movie={movie} />
+			))}
+		</div>
+	);
+}
+
+export async function getServerSideProps() {
+	const { API_URL } = process.env;
+
+	const res = await fetch(`${API_URL}/movies`);
+	const data = await res.json();
+
+	return {
+		props: {
+			movies: data,
+		},
+	};
 }
